37d3d185915e79da462f9dceeb363df8fa6f761e
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-12-05 12:30:06 -0300

    requirement 7 done

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+const CategoryModel = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          }
+    }, {
+        timestamps: false,
+    });
+    return Category;
+}
+
+module.exports = CategoryModel;
